b745d7de095d7096d94f27df0b56edb23508ba04
Simon Vidanovic <dvvidanovic@lbl.gov>
2017-09-16 12:11:28 -0700

    Clean warning message on gcc

--- src/Common/src/wceunique.hpp
+++ src/Common/src/wceunique.hpp
@@ -10,7 +10,7 @@ namespace std {
     template<typename T, typename... Args>
     std::unique_ptr<T> make_unique(Args... args) {
         return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-    };
+    }
 }
 
 #endif //WINDOWS_CALCENGINE_WCEUNIQUE_H
