39a775c404ebf76a2432ce1d0a6ffb3741d847ce
Simon Vidanovic <dvvidanovic@lbl.gov>
2018-05-31 16:47:00 -0700

    Removed some shared_ptr

--- src/Common/src/IntegratorStrategy.hpp
+++ src/Common/src/IntegratorStrategy.hpp
@@ -4,75 +4,77 @@
 #include <memory>
 #include <vector>
 
-namespace FenestrationCommon {
+namespace FenestrationCommon
+{
+    enum class IntegrationType
+    {
+        Rectangular,
+        RectangularCentroid,
+        Trapezoidal,
+        TrapezoidalA,
+        TrapezoidalB,
+        PreWeighted
+    };
 
-	enum class IntegrationType {
-		Rectangular, RectangularCentroid, Trapezoidal, TrapezoidalA, TrapezoidalB, PreWeighted
-	};
+    class ISeriesPoint;
 
-	class ISeriesPoint;
+    class CSeries;
 
-	class CSeries;
+    class IIntegratorStrategy
+    {
+    public:
+        virtual ~IIntegratorStrategy() = default;
 
-	class IIntegratorStrategy {
-	public:
-		virtual ~IIntegratorStrategy() = default;
+        // virtual double integrate( double const x1, double const x2, double const
+        // y1, double const y2 ) = 0;
+        virtual std::unique_ptr<CSeries> integrate(const std::vector<std::unique_ptr<ISeriesPoint>> & t_Series, double normalizationCoeff = 1) = 0;
 
-		// virtual double integrate( double const x1, double const x2, double const y1, double const y2 ) = 0;
-		virtual std::unique_ptr< CSeries >
-		integrate( const std::vector< std::unique_ptr< ISeriesPoint > > & t_Series,
-							 double normalizationCoeff = 1 ) = 0;
-	protected:
-		double dX( double x1, double x2 ) const;
-	};
+    protected:
+        double dX(double x1, double x2) const;
+    };
 
-	class CIntegratorRectangular : public IIntegratorStrategy {
-	public:
-		std::unique_ptr< CSeries >
-		integrate( const std::vector< std::unique_ptr< ISeriesPoint > > & t_Series,
-							 double normalizationCoeff ) override;
-	};
+    class CIntegratorRectangular : public IIntegratorStrategy
+    {
+    public:
+        std::unique_ptr<CSeries> integrate(const std::vector<std::unique_ptr<ISeriesPoint>> & t_Series, double normalizationCoeff) override;
+    };
 
-	class CIntegratorRectangularCentroid : public IIntegratorStrategy {
-	public:
-		std::unique_ptr< CSeries >
-		integrate( const std::vector< std::unique_ptr< ISeriesPoint > > & t_Series,
-							 double normalizationCoeff ) override;
-	};
+    class CIntegratorRectangularCentroid : public IIntegratorStrategy
+    {
+    public:
+        std::unique_ptr<CSeries> integrate(const std::vector<std::unique_ptr<ISeriesPoint>> & t_Series, double normalizationCoeff) override;
+    };
 
-	class CIntegratorTrapezoidal : public IIntegratorStrategy {
-	public:
-		std::unique_ptr< CSeries >
-		integrate( const std::vector< std::unique_ptr< ISeriesPoint > > & t_Series,
-							 double normalizationCoeff ) override;
-	};
+    class CIntegratorTrapezoidal : public IIntegratorStrategy
+    {
+    public:
+        std::unique_ptr<CSeries> integrate(const std::vector<std::unique_ptr<ISeriesPoint>> & t_Series, double normalizationCoeff) override;
+    };
 
-	class CIntegratorTrapezoidalA : public IIntegratorStrategy {
-	public:
-		std::unique_ptr< CSeries >
-		integrate( const std::vector< std::unique_ptr< ISeriesPoint > > & t_Series,
-							 double normalizationCoeff ) override;
-	};
+    class CIntegratorTrapezoidalA : public IIntegratorStrategy
+    {
+    public:
+        std::unique_ptr<CSeries> integrate(const std::vector<std::unique_ptr<ISeriesPoint>> & t_Series, double normalizationCoeff) override;
+    };
 
-	class CIntegratorTrapezoidalB : public IIntegratorStrategy {
-	public:
-		std::unique_ptr< CSeries >
-		integrate( const std::vector< std::unique_ptr< ISeriesPoint > > & t_Series,
-							 double normalizationCoeff ) override;
-	};
+    class CIntegratorTrapezoidalB : public IIntegratorStrategy
+    {
+    public:
+        std::unique_ptr<CSeries> integrate(const std::vector<std::unique_ptr<ISeriesPoint>> & t_Series, double normalizationCoeff) override;
+    };
 
-	class CIntegratorPreWeighted : public IIntegratorStrategy {
-	public:
-		std::unique_ptr< CSeries >
-		integrate( const std::vector< std::unique_ptr< ISeriesPoint > > & t_Series,
-							 double normalizationCoeff ) override;
-	};
+    class CIntegratorPreWeighted : public IIntegratorStrategy
+    {
+    public:
+        std::unique_ptr<CSeries> integrate(const std::vector<std::unique_ptr<ISeriesPoint>> & t_Series, double normalizationCoeff) override;
+    };
 
-	class CIntegratorFactory {
-	public:
-		std::shared_ptr< IIntegratorStrategy > getIntegrator( IntegrationType t_IntegratorType ) const;
-	};
+    class CIntegratorFactory
+    {
+    public:
+        std::unique_ptr<IIntegratorStrategy> getIntegrator(IntegrationType t_IntegratorType) const;
+    };
 
-}
+}   // namespace FenestrationCommon
 
 #endif
