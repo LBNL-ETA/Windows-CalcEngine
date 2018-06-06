c73e55aeaba3cb3e1f265824fdc2fc6c3572aeaa
Simon Vidanovic <dvvidanovic@lbl.gov>
2018-06-06 14:41:45 -0700

    Forward and backward layers are temporaries that are not stored in memory anymore (removed from private member).

--- src/MultiLayerOptics/src/MultiLayerInterRefSingleComponent.hpp
+++ src/MultiLayerOptics/src/MultiLayerInterRefSingleComponent.hpp
@@ -42,8 +42,7 @@ namespace MultiLayerOptics
                       const FenestrationCommon::Side t_Side = FenestrationCommon::Side::Back);
 
         // Retrieves value of energy to the surface of given layer. Incoming energy can be outside or inside
-        double
-          getEnergyToSurface(const size_t Index, const FenestrationCommon::Side t_Side, const FenestrationCommon::EnergyFlow t_EnergyFlow);
+        double getEnergyToSurface(const size_t Index, const FenestrationCommon::Side t_Side, const FenestrationCommon::EnergyFlow t_EnergyFlow);
 
         const CSurfaceEnergy & getSurfaceEnergy();
 
@@ -52,10 +51,6 @@ namespace MultiLayerOptics
     private:
         void initialize(const double t_Tf, const double t_Rf, const double t_Tb, const double t_Rb);
         void calculateEnergies();
-        void calculateForwardLayers();
-        void calculateBackwardLayers();
-
-        std::vector<SingleLayerOptics::CLayerSingleComponent> m_Layers;
 
         // Keeping equivalent optical properties while adding layers. This is necessary
         // for calculation of interreflectances.
@@ -66,14 +61,16 @@ namespace MultiLayerOptics
         // Third item in the std::vector is IGU made of first and second layer of the IGU.
         // Fourth item in the std::vector is complete IGU. So in this case, number of items in forward layers std::vector
         // will be four
-        std::vector<SingleLayerOptics::CLayerSingleComponent> m_ForwardLayers;
+        std::vector<SingleLayerOptics::CLayerSingleComponent> calculateForwardLayers();
 
         // Backward layer std::vector is build in opposite way.
         // First item in the std::vector is interior environment (transmittance is one and reflectance is zero).
         // Second item is third layer from the IGU.
         // Third item is IGU made of second and third layer of the IGU
         // Fourth item is complete IGU
-        std::vector<SingleLayerOptics::CLayerSingleComponent> m_BackwardLayers;
+        std::vector<SingleLayerOptics::CLayerSingleComponent> calculateBackwardLayers();
+
+        std::vector<SingleLayerOptics::CLayerSingleComponent> m_Layers;
 
         // Results of interreflectances are calculated for beam incoming from inside and outside
         CSurfaceEnergy m_IEnergy;
