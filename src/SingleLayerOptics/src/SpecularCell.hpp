cc419e62cf398a81b793e7cccd5168d48ec11fb4
Simon Vidanovic <dvvidanovic@lbl.gov>
2018-09-21 10:12:40 -0700

    Adds override for virtual functions in SpecularCell (new compiler warnings)

--- src/SingleLayerOptics/src/SpecularCell.hpp
+++ src/SingleLayerOptics/src/SpecularCell.hpp
@@ -29,19 +29,19 @@ namespace SingleLayerOptics {
 		explicit CSpecularCell( const std::shared_ptr< CMaterial >& t_MaterialProperties );
 
 		// Transmittance averaged over entire wavelength spectrum
-		double T_dir_dir( FenestrationCommon::Side t_Side, const CBeamDirection& t_Direction );
+		double T_dir_dir( FenestrationCommon::Side t_Side, const CBeamDirection& t_Direction ) override;
 
 		// Reflectance averaged over entire wavelength spectrum
-		double R_dir_dir( FenestrationCommon::Side t_Side, const CBeamDirection& t_Direction );
+		double R_dir_dir( FenestrationCommon::Side t_Side, const CBeamDirection& t_Direction ) override;
 
 		// Transmittance of specular material for enire wavelength spectrum
 		std::vector< double > T_dir_dir_band(
-			FenestrationCommon::Side t_Side, const CBeamDirection& t_Direction );
+			FenestrationCommon::Side t_Side, const CBeamDirection& t_Direction ) override;
 
 		// Reflectance of specular material over entire wavelength spectrum
 		std::vector< double > R_dir_dir_band(
 			FenestrationCommon::Side t_Side,
-			const CBeamDirection& t_Direction );
+			const CBeamDirection& t_Direction ) override;
 
 	protected:
 		std::shared_ptr< CSpecularCellDescription > getCellAsSpecular() const;
