b86afcac083ce819a2f7107c792a7fc8f2acfae1
Simon Vidanovic <dvvidanovic@lbl.gov>
2019-02-22 12:37:54 -0800

    Test case for U and SHGC runs added.

--- src/Tarcog/tst/DoubleClear_SHGCRun.unit.cpp
+++ src/Tarcog/tst/DoubleClear_SHGCRun.unit.cpp
@@ -6,7 +6,7 @@
 #include "WCECommon.hpp"
 
 // Example of double clear window with inital guess for solution
-class TestDoubleClear : public testing::Test
+class TestDoubleClearSHGCRun : public testing::Test
 {
 private:
     std::shared_ptr<Tarcog::ISO15099::CSystem> m_TarcogSystem;
@@ -17,10 +17,10 @@ protected:
         /////////////////////////////////////////////////////////
         /// Outdoor
         /////////////////////////////////////////////////////////
-        auto airTemperature = 255.15;   // Kelvins
-        auto airSpeed = 5.5;            // meters per second
-        auto tSky = 255.15;             // Kelvins
-        auto solarRadiation = 789.0;
+        auto airTemperature = 305.15;   // Kelvins
+        auto airSpeed = 2.75;           // meters per second
+        auto tSky = 305.15;             // Kelvins
+        auto solarRadiation = 783.0;
 
         auto Outdoor =
 			Tarcog::ISO15099::Environments::outdoor( airTemperature,
@@ -35,7 +35,7 @@ protected:
         /// Indoor
         /////////////////////////////////////////////////////////
 
-        auto roomTemperature = 294.15;
+        auto roomTemperature = 297.15;
 
         auto Indoor = Tarcog::ISO15099::Environments::indoor(roomTemperature);
         ASSERT_TRUE(Indoor != nullptr);
@@ -43,18 +43,18 @@ protected:
         /////////////////////////////////////////////////////////
         /// IGU
         /////////////////////////////////////////////////////////
-        auto solidLayerThickness = 0.005715;   // [m]
+        auto solidLayerThickness = 0.003048;   // [m]
         auto solidLayerConductance = 1.0;      // [W/m2K]
 
         auto aSolidLayer1 =
           Tarcog::ISO15099::Layers::solid(solidLayerThickness, solidLayerConductance);
-        aSolidLayer1->setSolarAbsorptance(0.166707709432);
+        aSolidLayer1->setSolarAbsorptance(0.096489921212);
 
         auto aSolidLayer2 =
           Tarcog::ISO15099::Layers::solid(solidLayerThickness, solidLayerConductance);
-        aSolidLayer2->setSolarAbsorptance(0.112737670541);
+        aSolidLayer2->setSolarAbsorptance(0.072256758809);
 
-        auto gapThickness = 0.012;
+        auto gapThickness = 0.0127;
         auto gapLayer = Tarcog::ISO15099::Layers::gap(gapThickness);
         ASSERT_TRUE(gapLayer != nullptr);
 
@@ -82,7 +82,7 @@ public:
     };
 };
 
-TEST_F(TestDoubleClear, Test1)
+TEST_F(TestDoubleClearSHGCRun, Test1)
 {
     SCOPED_TRACE("Begin Test: Double Clear - Surface temperatures");
 
@@ -96,7 +96,7 @@ TEST_F(TestDoubleClear, Test1)
     auto aRun = Tarcog::ISO15099::System::Uvalue;
 
     auto Temperature = aSystem->getTemperatures(aRun);
-    std::vector<double> correctTemperature = {258.756688, 259.359226, 279.178510, 279.781048};
+    std::vector<double> correctTemperature = {304.025047, 303.955156, 300.484758, 300.414867};
     ASSERT_EQ(correctTemperature.size(), Temperature.size());
 
     for(auto i = 0u; i < correctTemperature.size(); ++i)
@@ -105,7 +105,7 @@ TEST_F(TestDoubleClear, Test1)
     }
 
     auto Radiosity = aSystem->getRadiosities(aRun);
-    std::vector<double> correctRadiosity = {251.950834, 268.667346, 332.299338, 359.731700};
+    std::vector<double> correctRadiosity = {485.546128, 480.950664, 465.217923, 458.631346};
     ASSERT_EQ(correctRadiosity.size(), Radiosity.size());
     for(auto i = 0u; i < correctRadiosity.size(); ++i)
     {
@@ -113,7 +113,7 @@ TEST_F(TestDoubleClear, Test1)
     }
 
     auto numOfIter = aSystem->getNumberOfIterations(aRun);
-    EXPECT_EQ(20, int(numOfIter));
+    EXPECT_EQ(19, int(numOfIter));
 
     //////////////////////////////////////////////////////////////////////
     /// SHGC run
@@ -122,7 +122,7 @@ TEST_F(TestDoubleClear, Test1)
     aRun = Tarcog::ISO15099::System::SHGC;
 
     Temperature = aSystem->getTemperatures(aRun);
-    correctTemperature = {264.022835, 265.134421, 287.947300, 288.428857};
+    correctTemperature = {308.185604, 308.260088, 306.318284, 306.191403};
     ASSERT_EQ(correctTemperature.size(), Temperature.size());
 
     for(auto i = 0u; i < correctTemperature.size(); ++i)
@@ -131,7 +131,7 @@ TEST_F(TestDoubleClear, Test1)
     }
 
     Radiosity = aSystem->getRadiosities(aRun);
-    correctRadiosity = {269.869356, 295.289318, 374.655901, 397.518724};
+    correctRadiosity = {508.280530, 510.189512, 500.936293, 489.338313};
     ASSERT_EQ(correctRadiosity.size(), Radiosity.size());
 
     for(auto i = 0u; i < correctRadiosity.size(); ++i)
@@ -140,14 +140,14 @@ TEST_F(TestDoubleClear, Test1)
     }
 
     numOfIter = aSystem->getNumberOfIterations(aRun);
-    EXPECT_EQ(21, int(numOfIter));
+    EXPECT_EQ(20, int(numOfIter));
 
     //////////////////////////////////////////////////////////////////////
     /// General results
     //////////////////////////////////////////////////////////////////////
     auto Uvalue = aSystem->getUValue();
-    EXPECT_NEAR(Uvalue, 2.703359, 1e-5);
+    EXPECT_NEAR(Uvalue, 2.866261, 1e-5);
 
-    auto SHGC = aSystem->getSHGC(0.606897);
-    EXPECT_NEAR(SHGC, 0.690096, 1e-5);
+    auto SHGC = aSystem->getSHGC(0.703296);
+    EXPECT_NEAR(SHGC, 0.763304, 1e-5);
 }
