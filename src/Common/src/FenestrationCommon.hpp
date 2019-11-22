ab2e7e310320b4cc78be213e3a8d4f6770234ef8
Simon Vidanovic <dvvidanovic@lbl.gov>
2019-11-22 10:05:06 -0800

    Enumerator template separated into file.

--- src/Common/src/FenestrationCommon.hpp
+++ src/Common/src/FenestrationCommon.hpp
@@ -1,37 +1,9 @@
-#ifndef FENESTRATIONCOMMON_H
-#define FENESTRATIONCOMMON_H
+#pragma once
+
+#include "EnumerationTemplate.hpp"
 
 namespace FenestrationCommon
 {
-    template<typename T>
-    class Enum
-    {
-    public:
-        class Iterator
-        {
-        public:
-            explicit Iterator(int value) : m_value(value)
-            {}
-
-            T operator*(void)const
-            {
-                return static_cast<T>(m_value);
-            }
-
-            void operator++(void)
-            {
-                ++m_value;
-            }
-
-            bool operator!=(Iterator rhs)
-            {
-                return m_value != rhs.m_value;
-            }
-
-        private:
-            int m_value;
-        };
-    };
 
     template<typename T>
     int sgn(T val)
@@ -262,4 +234,3 @@ namespace FenestrationCommon
 
 }   // namespace FenestrationCommon
 
-#endif
