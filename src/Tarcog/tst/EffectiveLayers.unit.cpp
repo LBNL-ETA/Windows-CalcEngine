f38eed64b16281c507350b27a115bb1ceed88ac5
vidanovic <dvvidanovic@lbl.gov>
2020-04-17 13:35:22 -0700

    Vertical venetian unit test.

--- src/Tarcog/tst/EffectiveLayers.unit.cpp
+++ src/Tarcog/tst/EffectiveLayers.unit.cpp
@@ -59,3 +59,30 @@ TEST_F(TestEffectiveLayers, TestVenetianHorizontalEffectiveLayerWithTopAndBotOpe
     EXPECT_NEAR(0, effectiveOpenness.Al, 1e-8);
     EXPECT_NEAR(0, effectiveOpenness.Ar, 1e-8);
 }
+
+TEST_F(TestEffectiveLayers, TestVenetianVerticalEffectiveLayerWithTopAndBotOpenness)
+{
+    SCOPED_TRACE("Begin Test: Venetian horizontal effective layer properties.");
+
+    const auto width{1.3};                 // m
+    const auto height{1.8};                // m
+    const auto materialThickness{0.0001};   // m
+    const auto slatTiltAngle{0.0};
+    const auto slatWidth{0.0762};   // m
+
+    EffectiveLayers::ShadeOpenness openness{2.33584642410278, 0, 0, 0.01, 0.008};
+
+    EffectiveLayers::EffectiveVerticalVenentian venetian{
+      width, height, materialThickness, openness, slatTiltAngle, slatWidth};
+
+    const auto effectiveThickness{venetian.effectiveThickness()};
+
+    EXPECT_NEAR(9.144e-4, effectiveThickness, 1e-9);
+
+    const auto effectiveOpenness{venetian.getEffectiveOpenness()};
+    EXPECT_NEAR(9.589398567e-2, effectiveOpenness.Ah, 1e-8);
+    EXPECT_NEAR(1.3e-2, effectiveOpenness.Atop, 1e-8);
+    EXPECT_NEAR(1.04e-2, effectiveOpenness.Abot, 1e-8);
+    EXPECT_NEAR(0, effectiveOpenness.Al, 1e-8);
+    EXPECT_NEAR(0, effectiveOpenness.Ar, 1e-8);
+}
