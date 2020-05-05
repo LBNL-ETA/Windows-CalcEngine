efb5a9bb2252d44fdc83334b6ee942c5e4b1194a
vidanovic <dvvidanovic@lbl.gov>
2020-05-05 12:11:17 -0700

    Fix for scattering calculations in Woven shade.

--- src/SingleLayerOptics/tst/WovenShadeUniformMaterial.unit.cpp
+++ src/SingleLayerOptics/tst/WovenShadeUniformMaterial.unit.cpp
@@ -17,20 +17,20 @@ protected:
     virtual void SetUp()
     {
         // create material
-        const auto Tmat = 0.2;
-        const auto Rfmat = 0.75;
-        const auto Rbmat = 0.75;
-        const auto minLambda = 0.3;
-        const auto maxLambda = 2.5;
+        const auto Tmat = 0.0;
+        const auto Rfmat = 0.1;
+        const auto Rbmat = 0.1;
+        const auto minLambda = 5.0;
+        const auto maxLambda = 40.0;
         const auto aMaterial = SingleLayerOptics::Material::singleBandMaterial(
           Tmat, Tmat, Rfmat, Rbmat, minLambda, maxLambda);
 
         // make cell geometry
-        const auto diameter = 6.35;   // mm
-        const auto spacing = 19.05;   // mm
+        const auto diameter = 0.002;   // m
+        const auto spacing = 0.003;    // m
 
         // create BSDF
-        const auto aBSDF = CBSDFHemisphere::create(BSDFBasis::Quarter);
+        const auto aBSDF{CBSDFHemisphere::create(BSDFBasis::Quarter)};
 
         // make layer
         m_Shade = CBSDFLayerMaker::getWovenLayer(aMaterial, aBSDF, diameter, spacing);
@@ -52,11 +52,17 @@ TEST_F(TestWovenShadeUniformMaterial, TestSolarProperties)
     std::shared_ptr<CBSDFIntegrator> aResults = aShade->getResults();
 
     const double tauDiff = aResults->DiffDiff(Side::Front, PropertySimple::T);
-    EXPECT_NEAR(0.467579, tauDiff, 1e-6);
+    EXPECT_NEAR(0.037033896815761802, tauDiff, 1e-6);
 
     const double RfDiff = aResults->DiffDiff(Side::Front, PropertySimple::R);
-    EXPECT_NEAR(0.496269, RfDiff, 1e-6);
+    EXPECT_NEAR(0.096296610318422418, RfDiff, 1e-6);
 
     const double RbDiff = aResults->DiffDiff(Side::Back, PropertySimple::R);
-    EXPECT_NEAR(0.496269, RbDiff, 1e-6);
+    EXPECT_NEAR(0.096296610318422418, RbDiff, 1e-6);
+
+    const double theta{0.0};
+    const double phi{0.0};
+
+    const double Emiss{aResults->Abs(Side::Front, theta, phi)};
+    EXPECT_NEAR(0.8, Emiss, 1e-6);
 }
