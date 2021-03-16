0270e5897b551364b7807414e5a373f16e01d494
vidanovic <dvvidanovic@lbl.gov>
2021-03-16 16:37:36 -0700

    TSol no longer needed for CMA algorithm

--- src/Tarcog/tst/units/CMADoubleVisionHorizontalWindow.unit.cpp
+++ src/Tarcog/tst/units/CMADoubleVisionHorizontalWindow.unit.cpp
@@ -39,7 +39,6 @@ TEST_F(TestCMADoubleVisionHorizontalWindow, CMASingleVision)
     const auto UvalueCOG{1.258};
     const auto SHGCCOG{0.341};
     const auto tVis{0.535};
-    const auto tSol{0.3716};
     const auto spacerKeff{2.05280045621776};
 
     const double vt{window.vt(tVis)};
@@ -48,6 +47,6 @@ TEST_F(TestCMADoubleVisionHorizontalWindow, CMASingleVision)
     const double uvalue{window.uValue(UvalueCOG, spacerKeff)};
     EXPECT_NEAR(2.152215, uvalue, 1e-6);
 
-    const double windowSHGC{window.shgc(SHGCCOG, tSol, spacerKeff)};
+    const double windowSHGC{window.shgc(SHGCCOG, spacerKeff)};
     EXPECT_NEAR(0.272906, windowSHGC, 1e-6);
 }
