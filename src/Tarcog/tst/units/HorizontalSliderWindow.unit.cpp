1998e1c1934f67e65976e4de6a7e7fc1975334d6
vidanovic <dvvidanovic@lbl.gov>
2021-04-20 09:49:49 -0700

    Horizontal slider example added.

--- src/Tarcog/tst/units/HorizontalSliderWindow.unit.cpp
+++ src/Tarcog/tst/units/HorizontalSliderWindow.unit.cpp
@@ -2,16 +2,16 @@
 
 #include "WCETarcog.hpp"
 
-class TestSingleVisionWindow : public testing::Test
+class TestHorizontalSliderWindow : public testing::Test
 {
 protected:
     void SetUp() override
     {}
 };
 
-TEST_F(TestSingleVisionWindow, PredefinedCOGValues)
+TEST_F(TestHorizontalSliderWindow, PredefinedCOGValues)
 {
-    SCOPED_TRACE("Begin Test: Single vision window with predefined COG values.");
+    SCOPED_TRACE("Begin Test: Horizontal slider window.");
 
     const double uValue{2.134059};
     const double edgeUValue{2.251039};
@@ -30,24 +30,30 @@ TEST_F(TestSingleVisionWindow, PredefinedCOGValues)
     const auto tSol{0.3716};
     const auto hcout{15.0};
 
-    auto window = Tarcog::ISO15099::WindowSingleVision(
+    auto window = Tarcog::ISO15099::DualVisionHorizontal(
       width,
       height,
       tVis,
       tSol,
+      std::make_shared<Tarcog::ISO15099::SimpleIGU>(iguUValue, shgc, hcout),
+      tVis,
+      tSol,
       std::make_shared<Tarcog::ISO15099::SimpleIGU>(iguUValue, shgc, hcout));
 
-    window.setFrameTop(frameData);
-    window.setFrameBottom(frameData);
+    window.setFrameTopLeft(frameData);
+    window.setFrameTopRight(frameData);
+    window.setFrameMeetingRail(frameData);
     window.setFrameLeft(frameData);
     window.setFrameRight(frameData);
+    window.setFrameBottomLeft(frameData);
+    window.setFrameBottomRight(frameData);
 
     const double vt{window.vt()};
-    EXPECT_NEAR(0.544853, vt, 1e-6);
+    EXPECT_NEAR(0.519647, vt, 1e-6);
 
     const double uvalue{window.uValue()};
-    EXPECT_NEAR(1.833769, uvalue, 1e-6);
+    EXPECT_NEAR(1.902392, uvalue, 1e-6);
 
     const double windowSHGC{window.shgc()};
-    EXPECT_NEAR(0.373175, windowSHGC, 1e-6);
+    EXPECT_NEAR(0.357692, windowSHGC, 1e-6);
 }
