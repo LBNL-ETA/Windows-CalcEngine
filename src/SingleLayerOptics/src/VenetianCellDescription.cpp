2a94e1a83a43af488330a68f26dfc7458b1bfd73
vidanovic <dvvidanovic@lbl.gov>
2021-09-13 15:56:39 -0700

    Top and bottom slats in VenetianCellDescription are not longer pointers

--- src/SingleLayerOptics/src/VenetianCellDescription.cpp
+++ src/SingleLayerOptics/src/VenetianCellDescription.cpp
@@ -3,7 +3,6 @@
 #include <stdexcept>
 
 #include "VenetianCellDescription.hpp"
-#include "VenetianSlat.hpp"
 #include "BeamDirection.hpp"
 #include "WCECommon.hpp"
 
@@ -19,32 +18,34 @@ namespace SingleLayerOptics
         m_SlatTiltAngle(t_SlatTiltAngle),
         m_CurvatureRadius(t_CurvatureRadius),
         m_NumOfSegments(t_NumOfSlatSegments),
-        m_Top(std::make_shared<CVenetianSlat>(t_SlatWidth,
-                                              t_SlatSpacing,
-                                              t_SlatTiltAngle,
-                                              t_CurvatureRadius,
-                                              t_NumOfSlatSegments,
-                                              SegmentsDirection::Positive)),
-        m_Bottom(std::make_shared<CVenetianSlat>(t_SlatWidth,
-                                                 0,
-                                                 t_SlatTiltAngle,
-                                                 t_CurvatureRadius,
-                                                 t_NumOfSlatSegments,
-                                                 SegmentsDirection::Negative))
+        m_Top(t_SlatWidth,
+              t_SlatSpacing,
+              t_SlatTiltAngle,
+              t_CurvatureRadius,
+              t_NumOfSlatSegments,
+              SegmentsDirection::Positive),
+        m_Bottom(t_SlatWidth,
+                 0,
+                 t_SlatTiltAngle,
+                 t_CurvatureRadius,
+                 t_NumOfSlatSegments,
+                 SegmentsDirection::Negative)
     {
-        std::shared_ptr<Viewer::CViewSegment2D> exteriorSegment = std::make_shared<Viewer::CViewSegment2D>(
-          m_Bottom->geometry()->lastPoint(), m_Top->geometry()->firstPoint());
+        std::shared_ptr<Viewer::CViewSegment2D> exteriorSegment =
+          std::make_shared<Viewer::CViewSegment2D>(m_Bottom.geometry()->lastPoint(),
+                                                   m_Top.geometry()->firstPoint());
 
-        std::shared_ptr<Viewer::CViewSegment2D> interiorSegment = std::make_shared<Viewer::CViewSegment2D>(
-          m_Top->geometry()->lastPoint(), m_Bottom->geometry()->firstPoint());
+        std::shared_ptr<Viewer::CViewSegment2D> interiorSegment =
+          std::make_shared<Viewer::CViewSegment2D>(m_Top.geometry()->lastPoint(),
+                                                   m_Bottom.geometry()->firstPoint());
 
         m_Geometry.appendSegment(exteriorSegment);
-        m_Geometry.appendGeometry2D(m_Top->geometry());
+        m_Geometry.appendGeometry2D(m_Top.geometry());
         m_Geometry.appendSegment(interiorSegment);
-        m_Geometry.appendGeometry2D(m_Bottom->geometry());
+        m_Geometry.appendGeometry2D(m_Bottom.geometry());
 
-        m_BeamGeometry.appendGeometry2D(m_Top->geometry());
-        m_BeamGeometry.appendGeometry2D(m_Bottom->geometry());
+        m_BeamGeometry.appendGeometry2D(m_Top.geometry());
+        m_BeamGeometry.appendGeometry2D(m_Bottom.geometry());
     }
 
     size_t CVenetianCellDescription::numberOfSegments() const
@@ -52,7 +53,7 @@ namespace SingleLayerOptics
         assert(m_Top != nullptr);
         assert(m_Bottom != nullptr);
         // Two additional segments are for interior and exterior openess
-        return 2 + m_Top->numberOfSegments() + m_Bottom->numberOfSegments();
+        return 2 + m_Top.numberOfSegments() + m_Bottom.numberOfSegments();
     }
 
     double CVenetianCellDescription::segmentLength(const size_t Index) const
@@ -69,11 +70,11 @@ namespace SingleLayerOptics
 
     std::shared_ptr<CVenetianCellDescription> CVenetianCellDescription::makeBackwardCell() const
     {
-        double slatWidth = m_Top->slatWidth();
-        double slatSpacing = m_Top->slatSpacing();
-        double slatTiltAngle = -m_Top->slatTiltAngle();
-        double curvatureRadius = m_Top->curvatureRadius();
-        size_t m_NumOfSlatSegments = m_Top->numberOfSegments();
+        double slatWidth = m_Top.slatWidth();
+        double slatSpacing = m_Top.slatSpacing();
+        double slatTiltAngle = -m_Top.slatTiltAngle();
+        double curvatureRadius = m_Top.curvatureRadius();
+        size_t m_NumOfSlatSegments = m_Top.numberOfSegments();
 
         std::shared_ptr<CVenetianCellDescription> aBackwardCell =
           std::make_shared<CVenetianCellDescription>(
@@ -88,7 +89,8 @@ namespace SingleLayerOptics
     }
 
     std::shared_ptr<std::vector<Viewer::BeamViewFactor>>
-      CVenetianCellDescription::beamViewFactors(const double t_ProfileAngle, const FenestrationCommon::Side t_Side)
+      CVenetianCellDescription::beamViewFactors(const double t_ProfileAngle,
+                                                const FenestrationCommon::Side t_Side)
     {
         return m_BeamGeometry.beamViewFactors(-t_ProfileAngle, t_Side);
     }
@@ -101,7 +103,8 @@ namespace SingleLayerOptics
         return m_BeamGeometry.directToDirect(-aProfileAngle, t_Side);
     }
 
-    double CVenetianCellDescription::R_dir_dir(const FenestrationCommon::Side, const CBeamDirection &)
+    double CVenetianCellDescription::R_dir_dir(const FenestrationCommon::Side,
+                                               const CBeamDirection &)
     {
         return 0;
     }
