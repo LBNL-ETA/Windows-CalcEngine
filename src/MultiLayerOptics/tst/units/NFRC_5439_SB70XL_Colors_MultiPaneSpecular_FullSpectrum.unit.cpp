1dad8e471862da5e72d9b728bc3ea831a469f3dc
Simon Vidanovic <dvvidanovic@lbl.gov>
2022-08-04 10:59:41 -0700

    Color unit test is now using condensed spectrum example.

--- src/MultiLayerOptics/tst/units/NFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum.unit.cpp
+++ src/MultiLayerOptics/tst/units/NFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum.unit.cpp
@@ -6,12 +6,12 @@
 
 using FenestrationCommon::CSeries;
 
-class TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular : public testing::Test
+class TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum : public testing::Test
 {
 private:
     std::shared_ptr<SingleLayerOptics::ColorProperties> m_Color;
 
-    std::vector<double> loadWavelengths() const
+    static std::vector<double> loadWavelengths()
     {
         std::vector<double> aWavelengths{
           0.380, 0.385, 0.390, 0.395, 0.400, 0.405, 0.410, 0.415, 0.420, 0.425, 0.430, 0.435,
@@ -25,7 +25,7 @@ private:
         return aWavelengths;
     }
 
-    CSeries loadSolarRadiationFile() const
+    static CSeries loadSolarRadiationFile()
     {
         // Full CIE Illuminant D651 nm ssp table (used for PHOTOPIC properties)
         CSeries solarRadiation(
@@ -166,7 +166,7 @@ private:
         return solarRadiation;
     }
 
-    std::shared_ptr<SpectralAveraging::CSpectralSampleData> loadSampleData_NFRC_5439() const
+    [[nodiscard]] static std::shared_ptr<SpectralAveraging::CSpectralSampleData> loadSampleData_NFRC_5439()
     {
         return SpectralAveraging::CSpectralSampleData::create(
           {{0.300, 0.0019, 0.0491, 0.2686},  {0.305, 0.0037, 0.0885, 0.2723},
@@ -245,7 +245,7 @@ private:
            {40.000, 0.0000, 0.1381, 0.9915}});
     }
 
-    CSeries ASTM_E308_1964_X() const
+    static CSeries ASTM_E308_1964_X()
     {
         return CSeries(
           {{0.380, 0.0002}, {0.385, 0.0007}, {0.390, 0.0024}, {0.395, 0.0072}, {0.400, 0.0191},
@@ -267,7 +267,7 @@ private:
            {0.780, 0.0000}});
     }
 
-    CSeries ASTM_E308_1964_Y() const
+    static CSeries ASTM_E308_1964_Y()
     {
         return CSeries(
           {{0.380, 0.0000}, {0.385, 0.0001}, {0.390, 0.0003}, {0.395, 0.0008}, {0.400, 0.0020},
@@ -289,7 +289,7 @@ private:
            {0.780, 0.0000}});
     }
 
-    CSeries ASTM_E308_1964_Z() const
+    static CSeries ASTM_E308_1964_Z()
     {
         return CSeries(
           {{0.380, 0.0007}, {0.385, 0.0029}, {0.390, 0.0105}, {0.395, 0.0323}, {0.400, 0.0860},
@@ -311,7 +311,7 @@ private:
            {0.780, 0.0000}});
     }
 
-    std::unique_ptr<MultiLayerOptics::CMultiPaneSpecular>
+    [[nodiscard]] std::unique_ptr<MultiLayerOptics::CMultiPaneSpecular>
       createLayer(const CSeries & astmStandard) const
     {
         double thickness = 3.048e-3;   // [m]
@@ -321,8 +321,6 @@ private:
                                                      thickness,
                                                      FenestrationCommon::MaterialType::Monolithic,
                                                      FenestrationCommon::WavelengthRange::Visible);
-
-        aMaterial->setBandWavelengths(loadWavelengths());
 
         auto single_layer = SingleLayerOptics::SpecularLayer::createLayer(aMaterial);
 
@@ -351,10 +349,10 @@ protected:
 
         CSeries solarRadiation{loadSolarRadiationFile()};
 
-        auto wl = loadWavelengths();
+        //auto wl = loadWavelengths();
 
         m_Color = std::make_shared<SingleLayerOptics::ColorProperties>(
-          std::move(LayerX), std::move(LayerY), std::move(LayerZ), solarRadiation, DX, DY, DZ, wl);
+          std::move(LayerX), std::move(LayerY), std::move(LayerZ), solarRadiation, DX, DY, DZ);
     }
 
 public:
@@ -364,7 +362,7 @@ public:
     }
 };
 
-TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestTrichromatic_T)
+TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum, TestTrichromatic_T)
 {
     SCOPED_TRACE("Begin Test: Trichromatic.");
 
@@ -374,12 +372,12 @@ TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestTrichromatic_T)
 
     SingleLayerOptics::Trichromatic T = aLayer->getTrichromatic(
       FenestrationCommon::PropertySimple::T, aSide, FenestrationCommon::Scattering::DirectDirect);
-    EXPECT_NEAR(66.393144, T.X, 1e-6);
+    EXPECT_NEAR(66.445296720302537, T.X, 1e-6);
     EXPECT_NEAR(71.662457, T.Y, 1e-6);
-    EXPECT_NEAR(71.768345, T.Z, 1e-6);
+    EXPECT_NEAR(71.940385010574701, T.Z, 1e-6);
 }
 
-TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestTrichromatic_R)
+TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum, TestTrichromatic_R)
 {
     SCOPED_TRACE("Begin Test: Trichromatic.");
 
@@ -389,12 +387,12 @@ TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestTrichromatic_R)
 
     SingleLayerOptics::Trichromatic T = aLayer->getTrichromatic(
       FenestrationCommon::PropertySimple::R, aSide, FenestrationCommon::Scattering::DirectDirect);
-    EXPECT_NEAR(6.971494, T.X, 1e-6);
+    EXPECT_NEAR(6.976970444136497, T.X, 1e-6);
     EXPECT_NEAR(7.635557, T.Y, 1e-6);
-    EXPECT_NEAR(10.159147, T.Z, 1e-6);
+    EXPECT_NEAR(10.183499797132086, T.Z, 1e-6);
 }
 
-TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestRGB_T)
+TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum, TestRGB_T)
 {
     SCOPED_TRACE("Begin Test: RGB.");
 
@@ -409,7 +407,7 @@ TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestRGB_T)
     EXPECT_EQ(233, rgb.B);
 }
 
-TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestRGB_R)
+TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum, TestRGB_R)
 {
     SCOPED_TRACE("Begin Test: RGB.");
 
@@ -424,7 +422,7 @@ TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestRGB_R)
     EXPECT_EQ(96, rgb.B);
 }
 
-TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestCIE_LAB_T)
+TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum, TestCIE_LAB_T)
 {
     SCOPED_TRACE("Begin Test: CIE_LAB.");
 
@@ -439,7 +437,7 @@ TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestCIE_LAB_T)
     EXPECT_NEAR(4.081155, cie.b, 1e-6);
 }
 
-TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular, TestCIE_LAB_R)
+TEST_F(TestNFRC_5439_SB70XL_Colors_MultiPaneSpecular_FullSpectrum, TestCIE_LAB_R)
 {
     SCOPED_TRACE("Begin Test: CIE_LAB.");
 
