ea0f061fdda3087d39d8b6097ea0e105c565689b
Simon Vidanovic <dvvidanovic@lbl.gov>
2022-10-03 13:57:22 -0700

    Fastest execution is happening when number of threads that is used is maximum on the machine.

--- src/Common/src/Utility.cpp
+++ src/Common/src/Utility.cpp
@@ -32,7 +32,7 @@ namespace FenestrationCommon
 
     size_t getNumberOfThreads(size_t numberOfJobs)
     {
-        size_t numberOfThreads{std::min<size_t>(std::thread::hardware_concurrency(), static_cast<size_t>(numberOfJobs / 2))};
+        size_t numberOfThreads{std::min<size_t>(std::thread::hardware_concurrency(), static_cast<size_t>(numberOfJobs))};
         //static const size_t maxNumberOfThreads{32u};
         //numberOfThreads = std::min(numberOfThreads, maxNumberOfThreads);
         static const size_t minNumberOfThreads{1u};
