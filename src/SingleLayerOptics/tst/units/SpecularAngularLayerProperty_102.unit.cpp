0d49ff8ddab71509685bbe0715144beb1fe0b358
vidanovic <dvvidanovic@lbl.gov>
2022-10-14 13:42:46 -0700

    Update on two optical results.

--- src/SingleLayerOptics/tst/units/SpecularAngularLayerProperty_102.unit.cpp
+++ src/SingleLayerOptics/tst/units/SpecularAngularLayerProperty_102.unit.cpp
@@ -569,9 +569,9 @@ TEST_F(TestSpecularAngularLayerProperty_102, TestSpecularAngular10degree)
     double refleF = aMaterial->getProperty(Property::R, Side::Front, angle);
     double refleB = aMaterial->getProperty(Property::R, Side::Back, angle);
 
-    EXPECT_NEAR(0.84355467434102727, trans, 1e-6);
-    EXPECT_NEAR(0.074379357359260365, refleF, 1e-6);
-    EXPECT_NEAR(0.074379357359260365, refleB, 1e-6);
+    EXPECT_NEAR(0.84356824495725335, trans, 1e-6);
+    EXPECT_NEAR(0.074383187635746806, refleF, 1e-6);
+    EXPECT_NEAR(0.074383187635746806, refleB, 1e-6);
 }
 
 TEST_F(TestSpecularAngularLayerProperty_102, TestSpecularAngular25degree)
