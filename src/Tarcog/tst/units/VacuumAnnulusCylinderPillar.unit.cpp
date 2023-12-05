b88b770782d9122c7a46545a1b9b56ff957ea175
Simon Vidanovic <dvvidanovic@lbl.gov>
2023-12-05 12:40:31 -0800

    Complete IGU vacuum examples added for the mathematical models.

---
+++ src/Tarcog/tst/units/VacuumAnnulusCylinderPillar.unit.cpp
@@ -0,0 +1,126 @@
+#include <memory>
+#include <gtest/gtest.h>
+
+#include <WCETarcog.hpp>
+
+class DoubleLowEVacuumAnnulusCylinderPillar : public testing::Test
+{
+private:
+    std::shared_ptr<Tarcog::ISO15099::CSingleSystem> m_TarcogSystem;
+
+protected:
+    void SetUp() override
+    {
+        /////////////////////////////////////////////////////////
+        /// Outdoor
+        /////////////////////////////////////////////////////////
+        auto airTemperature = 255.15;   // Kelvins
+        auto airSpeed = 5.5;            // meters per second
+        auto tSky = 255.15;             // Kelvins
+        auto solarRadiation = 0.0;
+
+        auto Outdoor = Tarcog::ISO15099::Environments::outdoor(
+          airTemperature, airSpeed, solarRadiation, tSky, Tarcog::ISO15099::SkyModel::AllSpecified);
+        ASSERT_TRUE(Outdoor != nullptr);
+        Outdoor->setHCoeffModel(Tarcog::ISO15099::BoundaryConditionsCoeffModel::CalculateH);
+
+        /////////////////////////////////////////////////////////
+        /// Indoor
+        /////////////////////////////////////////////////////////
+
+        auto roomTemperature = 294.15;
+
+        auto Indoor = Tarcog::ISO15099::Environments::indoor(roomTemperature);
+        ASSERT_TRUE(Indoor != nullptr);
+
+        /////////////////////////////////////////////////////////
+        /// IGU
+        /////////////////////////////////////////////////////////
+        auto solidLayerThickness = 0.004;   // [m]
+        auto solidLayerConductance = 1.0;
+        auto TransmittanceIR = 0.0;
+        auto emissivityFrontIR = 0.84;
+        auto emissivityBackIR = 0.036749500781;
+
+        auto layer1 = Tarcog::ISO15099::Layers::solid(solidLayerThickness,
+                                                      solidLayerConductance,
+                                                      emissivityFrontIR,
+                                                      TransmittanceIR,
+                                                      emissivityBackIR,
+                                                      TransmittanceIR);
+
+        solidLayerThickness = 0.0039624;
+        emissivityBackIR = 0.84;
+
+        auto layer2 = Tarcog::ISO15099::Layers::solid(solidLayerThickness,
+                                                      solidLayerConductance,
+                                                      emissivityFrontIR,
+                                                      TransmittanceIR,
+                                                      emissivityBackIR,
+                                                      TransmittanceIR);
+
+        // Add support pillars
+        auto innerRadius1 = 0.1e-3;
+        auto outerRadius2 = 0.2e-3;
+        auto pillarHeight = 0.1e-3;
+        auto gapPressure = 0.1333;
+        auto pillarConductivity = 999.0;
+        auto pillarArea = 0.03 * 0.03;
+        auto pillarGap = Tarcog::ISO15099::Layers::annulusCylinderPillar(
+          innerRadius1, outerRadius2, pillarHeight, pillarConductivity, pillarArea, gapPressure);
+
+        ASSERT_TRUE(pillarGap != nullptr);
+
+        auto windowWidth = 1.0;   //[m]
+        auto windowHeight = 1.0;
+        Tarcog::ISO15099::CIGU aIGU(windowWidth, windowHeight);
+        aIGU.addLayers({layer1, pillarGap, layer2});
+
+        /////////////////////////////////////////////////////////
+        /// System
+        /////////////////////////////////////////////////////////
+        m_TarcogSystem = std::make_shared<Tarcog::ISO15099::CSingleSystem>(aIGU, Indoor, Outdoor);
+        ASSERT_TRUE(m_TarcogSystem != nullptr);
+
+        m_TarcogSystem->solve();
+    }
+
+public:
+    [[nodiscard]] std::shared_ptr<Tarcog::ISO15099::CSingleSystem> GetSystem() const
+    {
+        return m_TarcogSystem;
+    }
+};
+
+TEST_F(DoubleLowEVacuumAnnulusCylinderPillar, Test1)
+{
+    constexpr auto tolerance = 1e-6;
+
+    const auto aSystem = GetSystem();
+
+    ASSERT_TRUE(aSystem != nullptr);
+
+    const auto Temperature = aSystem->getTemperatures();
+    std::vector correctTemperature = {255.988242, 256.086082, 290.435380, 290.532300};
+    ASSERT_EQ(correctTemperature.size(), Temperature.size());
+
+    for(auto i = 0u; i < correctTemperature.size(); ++i)
+    {
+        EXPECT_NEAR(correctTemperature[i], Temperature[i], tolerance);
+    }
+
+    const auto Radiosity = aSystem->getRadiosities();
+    std::vector correctRadiosity = {242.959293, 396.487627, 402.311392, 407.239333};
+    ASSERT_EQ(correctRadiosity.size(), Radiosity.size());
+
+    for(auto i = 0u; i < correctRadiosity.size(); ++i)
+    {
+        EXPECT_NEAR(correctRadiosity[i], Radiosity[i], tolerance);
+    }
+
+    const auto numOfIter = aSystem->getNumberOfIterations();
+    EXPECT_EQ(26u, numOfIter);
+
+    const auto uValue{aSystem->getUValue()};
+    EXPECT_NEAR(0.627177, uValue, tolerance);
+}
