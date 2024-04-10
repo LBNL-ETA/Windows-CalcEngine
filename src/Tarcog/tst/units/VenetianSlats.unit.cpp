e9b6a36d2c3ebdf3f0302af1f32baccd4b114239
vidanovic <dvvidanovic@lbl.gov>
2024-04-09 18:56:20 -0700

    Cleanup of comments.

--- src/Tarcog/tst/units/VenetianSlats.unit.cpp
+++ src/Tarcog/tst/units/VenetianSlats.unit.cpp
@@ -10,7 +10,6 @@
 class TestVenetianSlats : public testing::Test
 {};
 
-// Testing calculateRise function
 TEST_F(TestVenetianSlats, RiseZeroCurvature)
 {
     EXPECT_NEAR(0.0, ThermalPermeability::Venetian::calculateRise(0, 10), 1e-6);
@@ -18,8 +17,6 @@ TEST_F(TestVenetianSlats, RiseZeroCurvature)
 
 TEST_F(TestVenetianSlats, RiseNegativeCurvature)
 {
-    // Assuming negative curvature is not expected, so result should be 0. Adjust if your
-    // implementation differs.
     EXPECT_NEAR(0.0, ThermalPermeability::Venetian::calculateRise(-10, 10), 1e-6);
 }
 
@@ -29,7 +26,6 @@ TEST_F(TestVenetianSlats, RiseStandardCondition)
     EXPECT_NEAR(expectedRise, ThermalPermeability::Venetian::calculateRise(5, 10), 1e-6);
 }
 
-// Testing calculateCurvature function
 TEST_F(TestVenetianSlats, CurvatureZeroRise)
 {
     EXPECT_NEAR(0.0, ThermalPermeability::Venetian::calculateCurvature(0, 10), 1e-6);
