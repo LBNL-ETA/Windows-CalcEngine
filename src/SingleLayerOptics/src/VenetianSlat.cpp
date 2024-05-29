831abfcaabb0053ef1f593d2ded7221af4991731
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-05-29 13:15:30 -0700

    buildSlats is using std::function to separate from a different cases instead of if-then-else.

--- src/SingleLayerOptics/src/VenetianSlat.cpp
+++ src/SingleLayerOptics/src/VenetianSlat.cpp
@@ -1,6 +1,7 @@
 #include <stdexcept>
 #include <cmath>
 #include <cassert>
+#include <functional>
 
 #include <WCEViewer.hpp>
 
@@ -13,41 +14,39 @@ namespace SingleLayerOptics
 {
     namespace Helper
     {
-        Viewer::CGeometry2D createPolarSegments(const VenetianGeometry & venetian,
-                                                size_t t_NumOfSegments,
-                                                SegmentsDirection t_Direction,
-                                                double radius)
+        double calculateTheta(const VenetianGeometry & venetian, double radius)
+        {
+            return 2 * std::asin(venetian.SlatWidth / (2 * radius));
+        }
+
+        std::pair<double, double> calculateThetaBounds(const VenetianGeometry & venetian,
+                                                       double theta)
         {
             using ConstantsData::WCE_PI;
+            return {degrees((venetian.CurvatureRadius > 0 ? WCE_PI / 2 : -WCE_PI / 2)
+                            + radians(venetian.SlatTiltAngle) - theta / 2),
+                    degrees((venetian.CurvatureRadius > 0 ? WCE_PI / 2 : -WCE_PI / 2)
+                            + radians(venetian.SlatTiltAngle) + theta / 2)};
+        }
 
-            double theta = 2 * std::asin(venetian.SlatWidth / (2 * radius));
-            double theta1 = 0;
-            double theta2 = 0;
-            double alpha = radians(venetian.SlatTiltAngle);
-
-            if(venetian.CurvatureRadius > 0)
-            {
-                theta1 = degrees(WCE_PI / 2 + alpha - theta / 2);
-                theta2 = degrees(WCE_PI / 2 + alpha + theta / 2);
-            }
-            else
-            {
-                theta1 = degrees(-WCE_PI / 2 + alpha + theta / 2);
-                theta2 = degrees(-WCE_PI / 2 + alpha - theta / 2);
-            }
+        Viewer::CGeometry2D createPolarSegments(const VenetianGeometry & venetian,
+                                                size_t t_NumOfSegments,
+                                                SegmentsDirection t_Direction)
+        {
+            auto [theta1, theta2]{calculateThetaBounds(venetian, calculateTheta(venetian, std::abs(venetian.CurvatureRadius)))};
 
             double dTheta = (theta2 - theta1) / t_NumOfSegments;
             double startTheta = t_Direction == SegmentsDirection::Positive ? theta2 : theta1;
 
-            auto startPoint{CPoint2D::createPointFromPolarCoordinates(startTheta, radius)};
+            auto startPoint{CPoint2D::createPointFromPolarCoordinates(startTheta, std::abs(venetian.CurvatureRadius))};
 
-            CGeometry2D aGeometry;
-            for(size_t i = 1; i <= t_NumOfSegments; ++i)
+            Viewer::CGeometry2D aGeometry;
+            for(size_t i = 1u; i <= t_NumOfSegments; ++i)
             {
                 double nextTheta = t_Direction == SegmentsDirection::Positive
                                      ? startTheta - dTheta * i
                                      : startTheta + dTheta * i;
-                const auto endPoint{CPoint2D::createPointFromPolarCoordinates(nextTheta, radius)};
+                const auto endPoint{CPoint2D::createPointFromPolarCoordinates(nextTheta, std::abs(venetian.CurvatureRadius))};
                 aGeometry.appendSegment({startPoint, endPoint});
                 startPoint = endPoint;
             }
@@ -92,27 +91,45 @@ namespace SingleLayerOptics
         }
     }   // namespace Helper
 
-    Viewer::CGeometry2D buildViewerSlat(const VenetianGeometry & venetian,
+    Viewer::CGeometry2D buildViewerSlat(const VenetianGeometry &venetian,
                                         size_t t_NumOfSegments,
                                         SegmentsDirection t_Direction)
     {
-        double radius = std::abs(venetian.CurvatureRadius);
+        // clang-format off
+        std::vector<std::pair<
+                std::function<bool(const VenetianGeometry &)>,
+                std::function<Viewer::CGeometry2D(const VenetianGeometry &, size_t, SegmentsDirection)>>
+        > conditionFunctionVector = {
+            {
+                [](const VenetianGeometry &ven)
+                {
+                    return std::abs(ven.CurvatureRadius) > (ven.SlatWidth / 2);
+                },
+                Helper::createPolarSegments
+            },
+            {
+                [](const VenetianGeometry &ven)
+                {
+                    return std::abs(ven.CurvatureRadius) == 0;
+                },
+                Helper::createCartesianSegments
+            }
+        };
+        // clang-format on
 
         Viewer::CGeometry2D aGeometry;
-        if(radius > (venetian.SlatWidth / 2))
-        {
-            aGeometry = Helper::createPolarSegments(venetian, t_NumOfSegments, t_Direction, radius);
+        for (const auto& [condition, func] : conditionFunctionVector) {
+            if (condition(venetian)) {
+                aGeometry = func(venetian, t_NumOfSegments, t_Direction);
+                break;
+            }
         }
-        else if(radius == 0)
-        {
-            aGeometry = Helper::createCartesianSegments(venetian, t_NumOfSegments, t_Direction);
-        }
-        else
-        {
+
+        if (aGeometry.segments().empty()) {
             throw std::runtime_error("Cannot create slat.");
         }
 
-        auto [translateX, translateY] = Helper::calculateTranslation(aGeometry, t_Direction);
+        auto [translateX, translateY]{Helper::calculateTranslation(aGeometry, t_Direction)};
         aGeometry = aGeometry.Translate(translateX, translateY + venetian.SlatSpacing);
 
         return aGeometry;
