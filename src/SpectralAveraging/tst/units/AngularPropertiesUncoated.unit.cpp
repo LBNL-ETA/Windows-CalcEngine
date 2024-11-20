1a06a9ac28a7195ea5522eac625bb49b96337595
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-11-20 10:02:52 -0800

    Fix for coating angular properties when T is small number close to zero.

--- src/SpectralAveraging/tst/units/AngularPropertiesUncoated.unit.cpp
+++ src/SpectralAveraging/tst/units/AngularPropertiesUncoated.unit.cpp
@@ -72,3 +72,32 @@ TEST_F(TestAngularPropertiesUncoated, Test2)
     EXPECT_NEAR(0, aProperties->transmittance(angle, lambda), 1e-6);
     EXPECT_NEAR(1, aProperties->reflectance(angle, lambda), 1e-6);
 }
+
+TEST_F(TestAngularPropertiesUncoated, Test3)
+{
+    SCOPED_TRACE("Begin Test: Uncoated properties - zero normal transmittance.");
+
+    auto aThickness = 0.005715;   // m
+    auto lambda = 0.8e-6;         // m
+    auto T0 = 1e-15;
+    auto R0 = 0.047;
+    auto angle = 0.0;
+
+    auto aAngularFactory = CAngularPropertiesFactory(T0, R0, aThickness);
+    auto aProperties = aAngularFactory.getAngularProperties(SurfaceType::Uncoated);
+
+    EXPECT_NEAR(0.0, aProperties->transmittance(angle, lambda), 1e-6);
+    EXPECT_NEAR(0.047, aProperties->reflectance(angle, lambda), 1e-6);
+
+    angle = 30;
+    EXPECT_NEAR(0.0, aProperties->transmittance(angle, lambda), 1e-6);
+    EXPECT_NEAR(0.048625638, aProperties->reflectance(angle, lambda), 1e-6);
+
+    angle = 60;
+    EXPECT_NEAR(0.0, aProperties->transmittance(angle, lambda), 1e-6);
+    EXPECT_NEAR(0.097922095, aProperties->reflectance(angle, lambda), 1e-6);
+
+    angle = 90;
+    EXPECT_NEAR(0, aProperties->transmittance(angle, lambda), 1e-6);
+    EXPECT_NEAR(1, aProperties->reflectance(angle, lambda), 1e-6);
+}
