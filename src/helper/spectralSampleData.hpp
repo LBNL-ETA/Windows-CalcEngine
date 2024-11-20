8b4c5c8b52f1f7d252b69bacd5d39160a2d7cc9f
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-11-20 10:57:50 -0800

    Helper files for standards data and spectral samples created.

---
+++ src/helper/spectralSampleData.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <WCESpectralAveraging.hpp>
+
+namespace SpectralSample
+{
+    std::shared_ptr<SpectralAveraging::CSpectralSampleData> NFRC_102();
+}
---
+++ src/helper/standardData.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <WCECommon.hpp>
+
+namespace StandardData
+{
+    FenestrationCommon::CSeries solarRadiationASTM_E891_87_Table1();
+}   // namespace StandardData
