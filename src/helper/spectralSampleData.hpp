f98cefcf2ddf2336831d7831c32d5f0fc9758093
Simon Vidanovic <dvvidanovic@lbl.gov>
2024-11-20 16:08:25 -0800

    Venetian blind while slat material added.

--- src/helper/spectralSampleData.hpp
+++ src/helper/spectralSampleData.hpp
@@ -5,4 +5,6 @@
 namespace SpectralSample
 {
     std::shared_ptr<SpectralAveraging::CSpectralSampleData> NFRC_102();
+
+    std::shared_ptr<SpectralAveraging::CSpectralSampleData> WhiteVenetianBlindSlat_31100();
 }
