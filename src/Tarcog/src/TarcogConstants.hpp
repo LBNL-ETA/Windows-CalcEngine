3315665dae607a1f6b1c683177679300e1b0f7b1
Dragan Vidanovic <DVVidanovic@lbl.gov>
2025-07-02 10:25:39 -0700

    Fix for gcc compiler

--- src/Tarcog/src/TarcogConstants.hpp
+++ src/Tarcog/src/TarcogConstants.hpp
@@ -2,42 +2,40 @@
 
 #include <limits>
 
+
 namespace Tarcog
 {
     namespace TarcogConstants
     {
-        const double DEFAULT_WINDOW_WIDTH = 1;
-        const double DEFAULT_WINDOW_HEIGHT = 1;
-        // Brackets around the following constant are necessary to avoid collision with the macro
-        // defined in the Windows API
-        const double DEFAULT_LAYER_THICKNESS = (std::numeric_limits<double>::max)();
-        const double DEFAULT_TILT = 90;
-        const double DEFAULT_ENV_EMISSIVITY = 1;
-        const double DEFAULT_FRACTION_OF_CLEAR_SKY = 1;
-    }   // namespace TarcogConstants
+        constexpr double DEFAULT_WINDOW_WIDTH = 1;
+        constexpr double DEFAULT_WINDOW_HEIGHT = 1;
+        constexpr double DEFAULT_LAYER_THICKNESS = (std::numeric_limits<double>::max)();
+        constexpr double DEFAULT_TILT = 90;
+        constexpr double DEFAULT_ENV_EMISSIVITY = 1;
+        constexpr double DEFAULT_FRACTION_OF_CLEAR_SKY = 1;
+    }
 
     namespace IterationConstants
     {
-        const double RELAXATION_PARAMETER_MAX = 0.65;
-        const double RELAXATION_PARAMETER_MIN = 0.05;
-        const double RELAXATION_PARAMETER_STEP = 0.05;
-        const double CONVERGENCE_TOLERANCE = 1e-8;
-        const size_t NUMBER_OF_STEPS = 200;
-        const double RELAXATION_PARAMETER_AIRFLOW = 0.9;
-        const double RELAXATION_PARAMETER_AIRFLOW_MIN = 0.1;
-        const double RELAXATION_PARAMETER_AIRFLOW_STEP = 0.1;
-        const double CONVERGENCE_TOLERANCE_AIRFLOW = 1e-2;
-    }   // namespace IterationConstants
+        constexpr double RELAXATION_PARAMETER_MAX = 0.65;
+        constexpr double RELAXATION_PARAMETER_MIN = 0.05;
+        constexpr double RELAXATION_PARAMETER_STEP = 0.05;
+        constexpr double CONVERGENCE_TOLERANCE = 1e-8;
+        constexpr size_t NUMBER_OF_STEPS = 200;
+        constexpr double RELAXATION_PARAMETER_AIRFLOW = 0.9;
+        constexpr double RELAXATION_PARAMETER_AIRFLOW_MIN = 0.1;
+        constexpr double RELAXATION_PARAMETER_AIRFLOW_STEP = 0.1;
+        constexpr double CONVERGENCE_TOLERANCE_AIRFLOW = 1e-2;
+    }
 
     namespace MaterialConstants
     {
-        const double GLASSDENSITY = 2500;
+        constexpr double GLASSDENSITY = 2500;
     }
 
     namespace DeflectionConstants
     {
-        const double YOUNGSMODULUS = 7.2e10;
-        const double POISONRATIO = 0.22;   // This constant is for glass
-    }                                      // namespace DeflectionConstants
-
-}   // namespace Tarcog
+        constexpr double YOUNGSMODULUS = 7.2e10;
+        constexpr double POISONRATIO = 0.22;
+    }
+}
