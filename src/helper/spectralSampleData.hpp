39a053b257023aa0e6b58132151ee1815442f8f2
Dragan Vidanovic <DVVidanovic@lbl.gov>
2025-08-14 14:56:28 -0700

    NFRC_VE348 moved to common input data

--- src/helper/spectralSampleData.hpp
+++ src/helper/spectralSampleData.hpp
@@ -8,4 +8,6 @@ namespace SpectralSample
     std::shared_ptr<SpectralAveraging::CSpectralSampleData> NFRC_103();
 
     std::shared_ptr<SpectralAveraging::CSpectralSampleData> WhiteVenetianBlindSlat_31100();
+
+    std::shared_ptr<SpectralAveraging::CSpectralSampleData> NFRC_VE348();
 }
