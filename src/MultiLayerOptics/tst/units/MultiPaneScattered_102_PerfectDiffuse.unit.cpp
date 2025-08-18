50581d96e6419e9d876f3ecbd38430f0bd8708ff
Dragan Vidanovic <DVVidanovic@lbl.gov>
2025-08-18 12:02:09 -0700

    MultiPaneScattered_102_PerfectDiffuse.unit.cpp

--- src/MultiLayerOptics/tst/units/MultiPaneScattered_102_PerfectDiffuse.unit.cpp
+++ src/MultiLayerOptics/tst/units/MultiPaneScattered_102_PerfectDiffuse.unit.cpp
@@ -1,11 +1,13 @@
 #include <memory>
 #include <gtest/gtest.h>
 
-#include "WCESpectralAveraging.hpp"
-#include "WCEMultiLayerOptics.hpp"
-#include "WCESingleLayerOptics.hpp"
-#include "WCECommon.hpp"
+#include <WCESpectralAveraging.hpp>
+#include <WCEMultiLayerOptics.hpp>
+#include <WCESingleLayerOptics.hpp>
+#include <WCECommon.hpp>
 
+#include "standardData.hpp"
+#include "spectralSampleData.hpp"
 
 using namespace SingleLayerOptics;
 using namespace FenestrationCommon;
@@ -16,43 +18,7 @@ using namespace MultiLayerOptics;
 
 class MultiPaneScattered_102_PerfectDiffuse : public testing::Test
 {
-private:
     std::unique_ptr<CMultiLayerScattered> m_Layer;
-
-    CSeries loadSolarRadiationFile()
-    {
-        // Full ASTM E891-87 Table 1 (Solar radiation)
-        CSeries aSolarRadiation(
-          {{0.3000, 0.0},    {0.3050, 3.4},    {0.3100, 15.6},   {0.3150, 41.1},   {0.3200, 71.2},
-           {0.3250, 100.2},  {0.3300, 152.4},  {0.3350, 155.6},  {0.3400, 179.4},  {0.3450, 186.7},
-           {0.3500, 212.0},  {0.3600, 240.5},  {0.3700, 324.0},  {0.3800, 362.4},  {0.3900, 381.7},
-           {0.4000, 556.0},  {0.4100, 656.3},  {0.4200, 690.8},  {0.4300, 641.9},  {0.4400, 798.5},
-           {0.4500, 956.6},  {0.4600, 990.0},  {0.4700, 998.0},  {0.4800, 1046.1}, {0.4900, 1005.1},
-           {0.5000, 1026.7}, {0.5100, 1066.7}, {0.5200, 1011.5}, {0.5300, 1084.9}, {0.5400, 1082.4},
-           {0.5500, 1102.2}, {0.5700, 1087.4}, {0.5900, 1024.3}, {0.6100, 1088.8}, {0.6300, 1062.1},
-           {0.6500, 1061.7}, {0.6700, 1046.2}, {0.6900, 859.2},  {0.7100, 1002.4}, {0.7180, 816.9},
-           {0.7244, 842.8},  {0.7400, 971.0},  {0.7525, 956.3},  {0.7575, 942.2},  {0.7625, 524.8},
-           {0.7675, 830.7},  {0.7800, 908.9},  {0.8000, 873.4},  {0.8160, 712.0},  {0.8237, 660.2},
-           {0.8315, 765.5},  {0.8400, 799.8},  {0.8600, 815.2},  {0.8800, 778.3},  {0.9050, 630.4},
-           {0.9150, 565.2},  {0.9250, 586.4},  {0.9300, 348.1},  {0.9370, 224.2},  {0.9480, 271.4},
-           {0.9650, 451.2},  {0.9800, 549.7},  {0.9935, 630.1},  {1.0400, 582.9},  {1.0700, 539.7},
-           {1.1000, 366.2},  {1.1200, 98.1},   {1.1300, 169.5},  {1.1370, 118.7},  {1.1610, 301.9},
-           {1.1800, 406.8},  {1.2000, 375.2},  {1.2350, 423.6},  {1.2900, 365.7},  {1.3200, 223.4},
-           {1.3500, 30.1},   {1.3950, 1.4},    {1.4425, 51.6},   {1.4625, 97.0},   {1.4770, 97.3},
-           {1.4970, 167.1},  {1.5200, 239.3},  {1.5390, 248.8},  {1.5580, 249.3},  {1.5780, 222.3},
-           {1.5920, 227.3},  {1.6100, 210.5},  {1.6300, 224.7},  {1.6460, 215.9},  {1.6780, 202.8},
-           {1.7400, 158.2},  {1.8000, 28.6},   {1.8600, 1.8},    {1.9200, 1.1},    {1.9600, 19.7},
-           {1.9850, 84.9},   {2.0050, 25.0},   {2.0350, 92.5},   {2.0650, 56.3},   {2.1000, 82.7},
-           {2.1480, 76.2},   {2.1980, 66.4},   {2.2700, 65.0},   {2.3600, 57.6},   {2.4500, 19.8},
-           {2.4940, 17.0},   {2.5370, 3.0},    {2.9410, 4.0},    {2.9730, 7.0},    {3.0050, 6.0},
-           {3.0560, 3.0},    {3.1320, 5.0},    {3.1560, 18.0},   {3.2040, 1.2},    {3.2450, 3.0},
-           {3.3170, 12.0},   {3.3440, 3.0},    {3.4500, 12.2},   {3.5730, 11.0},   {3.7650, 9.0},
-           {4.0450, 6.9}
-
-          });
-
-        return aSolarRadiation;
-    }
 
     std::shared_ptr<CSpectralSampleData> loadSampleData_NFRC_102()
     {
@@ -121,19 +87,19 @@ protected:
     void SetUp() override
     {
         // Create material from samples
-        auto thickness = 3.048e-3;   // [m]
-        auto aMaterial_102 = SingleLayerOptics::Material::nBandMaterial(
-          loadSampleData_NFRC_102(), thickness, MaterialType::Monolithic);
+        constexpr auto thickness = 3.048e-3;   // [m]
+        auto aMaterial_102 = Material::nBandMaterial(
+          SpectralSample::NFRC_102(), thickness, MaterialType::Monolithic);
 
         // Solar range
-        const auto Tsol = 0.0;
-        const auto Rfsol = 0.2;
-        const auto Rbsol = 0.2;
+        constexpr auto Tsol = 0.0;
+        constexpr auto Rfsol = 0.2;
+        constexpr auto Rbsol = 0.2;
 
         // Visible range
-        const auto Tvis = 0.0;
-        const auto Rfvis = 0.1;
-        const auto Rbvis = 0.1;
+        constexpr auto Tvis = 0.0;
+        constexpr auto Rfvis = 0.1;
+        constexpr auto Rbvis = 0.1;
 
         auto aMaterial = SingleLayerOptics::Material::dualBandMaterial(
           Tsol, Tsol, Rfsol, Rbsol, Tvis, Tvis, Rfvis, Rbvis);
@@ -146,12 +112,11 @@ protected:
         m_Layer = CMultiLayerScattered::create(Layer102);
         m_Layer->addLayer(LayerDiffuse);
 
-        CSeries solarRadiation{loadSolarRadiationFile()};
-        m_Layer->setSourceData(solarRadiation);
+        m_Layer->setSourceData(StandardData::solarRadiationASTM_E891_87_Table1());
     }
 
 public:
-    CMultiLayerScattered & getLayer() const
+    [[nodiscard]] CMultiLayerScattered & getLayer() const
     {
         return *m_Layer;
     };
@@ -162,14 +127,14 @@ TEST_F(MultiPaneScattered_102_PerfectDiffuse, TestPerfectDiffuseDirectBeam)
     SCOPED_TRACE(
       "Begin Test: Perfectly diffusing layer - Scattering model front side (normal incidence).");
 
-    const double minLambda = 0.3;
-    const double maxLambda = 2.5;
+    constexpr double minLambda = 0.3;
+    constexpr double maxLambda = 2.5;
 
     auto & aLayer = getLayer();
 
-    auto aSide = Side::Front;
-    double theta = 0;
-    double phi = 0;
+    constexpr auto aSide = Side::Front;
+    constexpr double theta = 0;
+    constexpr double phi = 0;
 
     auto T_dir_dir = aLayer.getPropertySimple(
       minLambda, maxLambda, PropertySimple::T, aSide, Scattering::DirectDirect, theta, phi);
@@ -213,14 +178,14 @@ TEST_F(MultiPaneScattered_102_PerfectDiffuse, TestPerfectDiffuseAngledBeam25)
     SCOPED_TRACE(
       "Begin Test: Perfectly diffusing layer - Scattering model back side (Theta = 25 deg).");
 
-    const double minLambda = 0.3;
-    const double maxLambda = 2.5;
+    constexpr double minLambda = 0.3;
+    constexpr double maxLambda = 2.5;
 
     auto & aLayer = getLayer();
 
-    auto aSide = Side::Front;
-    double theta = 25;
-    double phi = 0;
+    constexpr auto aSide = Side::Front;
+    constexpr double theta = 25;
+    constexpr double phi = 0;
 
     auto T_dir_dir = aLayer.getPropertySimple(
       minLambda, maxLambda, PropertySimple::T, aSide, Scattering::DirectDirect, theta, phi);
@@ -264,14 +229,14 @@ TEST_F(MultiPaneScattered_102_PerfectDiffuse, TestPerfectDiffuseAngleBeam50)
     SCOPED_TRACE(
       "Begin Test: Perfectly diffusing layer - Scattering model front side (Theta = 50 deg).");
 
-    const double minLambda = 0.3;
-    const double maxLambda = 2.5;
+    constexpr double minLambda = 0.3;
+    constexpr double maxLambda = 2.5;
 
     auto & aLayer = getLayer();
 
-    auto aSide = Side::Front;
-    double theta = 50;
-    double phi = 0;
+    constexpr auto aSide = Side::Front;
+    constexpr double theta = 50;
+    constexpr double phi = 0;
 
     auto T_dir_dir = aLayer.getPropertySimple(
       minLambda, maxLambda, PropertySimple::T, aSide, Scattering::DirectDirect, theta, phi);
