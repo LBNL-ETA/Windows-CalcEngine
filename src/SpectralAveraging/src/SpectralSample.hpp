10a59f2110e3237461fffafed9f008f46d1e6323
Dragan Vidanovic <DVVidanovic@lbl.gov>
2025-08-22 07:56:45 -0700

    TODO comments for calculating direct and diffuse portions of energy added

--- src/SpectralAveraging/src/SpectralSample.hpp
+++ src/SpectralAveraging/src/SpectralSample.hpp
@@ -92,6 +92,9 @@ namespace SpectralAveraging
         std::vector<double> m_Wavelengths;
         WavelengthSet m_WavelengthSet;
 
+        // TODO: EnergySource now need to contain energy from diffuse, direct and total. This is
+        // needed in order to give correct answer to outgoing energy in certain direction
+
         // Keep energy for current state of the sample. Energy is calculated for each wavelength.
         FenestrationCommon::CSeries m_IncomingSource;
         std::map<std::pair<FenestrationCommon::Property, FenestrationCommon::Side>,
