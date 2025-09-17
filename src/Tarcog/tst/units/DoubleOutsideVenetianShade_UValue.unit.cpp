101ff6825471211f7d5ca64616cdbd660fb7623d
Dragan Vidanovic <DVVidanovic@lbl.gov>
2025-09-17 10:56:27 -0700

    DoubleOutsideVenetianShade_UValue test cleanup

--- src/Tarcog/tst/units/DoubleOutsideVenetianShade_UValue.unit.cpp
+++ src/Tarcog/tst/units/DoubleOutsideVenetianShade_UValue.unit.cpp
@@ -2,13 +2,12 @@
 #include <stdexcept>
 #include <gtest/gtest.h>
 
-#include "WCEGases.hpp"
-#include "WCETarcog.hpp"
-#include "WCECommon.hpp"
+#include <WCEGases.hpp>
+#include <WCETarcog.hpp>
+#include <WCECommon.hpp>
 
 class TestDoubleOutsideVenetianShade_UValue : public testing::Test
 {
-private:
     std::unique_ptr<Tarcog::ISO15099::CSystem> m_TarcogSystem;
 
 protected:
@@ -17,10 +16,10 @@ protected:
         /////////////////////////////////////////////////////////
         /// Outdoor
         /////////////////////////////////////////////////////////
-        auto airTemperature = 255.15;   // Kelvins
-        auto airSpeed = 5.5;            // meters per second
-        auto tSky = 255.15;             // Kelvins
-        auto solarRadiation = 0.0;
+        constexpr auto airTemperature = 255.15;   // Kelvins
+        constexpr auto airSpeed = 5.5;            // meters per second
+        constexpr auto tSky = 255.15;             // Kelvins
+        constexpr auto solarRadiation = 0.0;
 
         auto Outdoor = Tarcog::ISO15099::Environments::outdoor(
           airTemperature, airSpeed, solarRadiation, tSky, Tarcog::ISO15099::SkyModel::AllSpecified);
@@ -31,7 +30,7 @@ protected:
         /// Indoor
         /////////////////////////////////////////////////////////
 
-        auto roomTemperature = 294.15;
+        constexpr auto roomTemperature = 294.15;
 
         auto Indoor = Tarcog::ISO15099::Environments::indoor(roomTemperature);
         ASSERT_TRUE(Indoor != nullptr);
@@ -39,27 +38,22 @@ protected:
         /////////////////////////////////////////////////////////
         /// IGU
         /////////////////////////////////////////////////////////
-        auto shadeLayerConductance = 160.0;
+        constexpr auto shadeLayerConductance = 160.0;
 
-        const auto matThickness{0.0001};   // m
+        constexpr auto matThickness{0.0001};   // m
 
-        const FenestrationCommon::Venetian::Geometry aGeometry{
-          0.0148, 0.0127, 0.0, 0.0331305656433105};
+        constexpr FenestrationCommon::Venetian::Geometry aGeometry{.SlatWidth = 0.0148,
+                                                                   .SlatSpacing = 0.0127,
+                                                                   .SlatTiltAngle = 0.0,
+                                                                   .CurvatureRadius =
+                                                                     0.0331305656433105};
 
-        const auto dl{0.0};
-        const auto dr{0.0};
-        const auto dtop{0.0};
-        const auto dbot{0.0};
+        EffectiveLayers::EffectiveHorizontalVenetian effectiveVenetian{matThickness, aGeometry};
 
-        EffectiveLayers::ShadeOpenness openness{dl, dr, dtop, dbot};
-
-        EffectiveLayers::EffectiveHorizontalVenetian effectiveVenetian{
-          matThickness, aGeometry, openness};
-
-        auto Ef = 0.5564947806702053;
-        auto Eb = 0.5564947806702053;
-        auto Tirf = 0.42293224373137134;
-        auto Tirb = 0.42293224373137134;
+        constexpr auto Ef = 0.5564947806702053;
+        constexpr auto Eb = 0.5564947806702053;
+        constexpr auto Tirf = 0.42293224373137134;
+        constexpr auto Tirb = 0.42293224373137134;
 
         auto aLayer1 = Tarcog::ISO15099::Layers::shading(effectiveVenetian.effectiveThickness(),
                                                          shadeLayerConductance,
@@ -69,17 +63,12 @@ protected:
                                                          Eb,
                                                          Tirb);
 
-        // auto aLayer1 = Tarcog::ISO15099::Layers::solid(shadeLayerThickness,
-        // shadeLayerConductance);
-
-        // aLayer1->setSolarHeatGain(0.306310117245, solarRadiation);
-
-        auto gapThickness = 0.0127;
+        constexpr auto gapThickness = 0.0127;
         auto GapLayer1 = Tarcog::ISO15099::Layers::gap(gapThickness);
         ASSERT_TRUE(GapLayer1 != nullptr);
 
-        auto solidLayerThickness = 0.003048;   // [m]
-        auto solidLayerConductance = 1.0;
+        constexpr auto solidLayerThickness = 0.003048;   // [m]
+        constexpr auto solidLayerConductance = 1.0;
         auto aLayer2 = Tarcog::ISO15099::Layers::solid(solidLayerThickness, solidLayerConductance);
         ASSERT_TRUE(aLayer2 != nullptr);
 
@@ -98,36 +87,35 @@ protected:
     }
 
 public:
-    Tarcog::ISO15099::CSystem * GetSystem() const
+    [[nodiscard]] Tarcog::ISO15099::CSystem & GetSystem() const
     {
-        return m_TarcogSystem.get();
-    };
+        return *m_TarcogSystem;
+    }
 };
 
 TEST_F(TestDoubleOutsideVenetianShade_UValue, Test1)
 {
     SCOPED_TRACE("Begin Test: Outside venetian shade.");
 
-    const auto aSystem = GetSystem();
+    auto aSystem = GetSystem();
 
     const auto effectiveLayerConductivities{
-      aSystem->getSolidEffectiveLayerConductivities(Tarcog::ISO15099::System::Uvalue)};
+      aSystem.getSolidEffectiveLayerConductivities(Tarcog::ISO15099::System::Uvalue)};
 
-    const std::vector<double> correctEffectConductivites{1.878064, 1};
+    const std::vector correctEffectConductivites{1.878064, 1.0};
     EXPECT_EQ(correctEffectConductivites.size(), effectiveLayerConductivities.size());
     for(size_t i = 0u; i < correctEffectConductivites.size(); ++i)
     {
         EXPECT_NEAR(correctEffectConductivites[i], effectiveLayerConductivities[i], 1e-6);
     }
 
-    const auto systemKeff{
-      aSystem->getEffectiveSystemConductivity(Tarcog::ISO15099::System::Uvalue)};
+    const auto systemKeff{aSystem.getEffectiveSystemConductivity(Tarcog::ISO15099::System::Uvalue)};
     EXPECT_NEAR(0.106427, systemKeff, 1e-6);
 
-    const auto uval = aSystem->getUValue();
+    const auto uval = aSystem.getUValue();
     EXPECT_NEAR(3.239692, uval, 1e-6);
 
     const auto heatflow =
-      aSystem->getHeatFlow(Tarcog::ISO15099::System::Uvalue, Tarcog::ISO15099::Environment::Indoor);
+      aSystem.getHeatFlow(Tarcog::ISO15099::System::Uvalue, Tarcog::ISO15099::Environment::Indoor);
     EXPECT_NEAR(126.347983, heatflow, 1e-6);
 }
